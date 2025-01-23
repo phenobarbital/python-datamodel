2b53e2c7780153303ec728a680a8e80d492af1d6
Jesus Lara <jesuslarag@gmail.com>
2025-01-23 19:57:23 +0100

    fix an segfault for infinite iteration on parse_type

--- examples/test_descriptor.py
+++ examples/test_descriptor.py
@@ -1,9 +1,9 @@
-from dataclasses import dataclass, field
-from datamodel import BaseModel
+from datamodel import BaseModel, Field
 
-class IntConversionDescriptor:
-    def __init__(self, *, default):
+class IntConversionDescriptor(Field):
+    def __init__(self, *, default, **kwargs):
         self._default = default
+        super().__init__(default=default, **kwargs)
 
     def __set_name__(self, owner, name):
         self._name = "_" + name
@@ -16,11 +16,23 @@ class IntConversionDescriptor:
     def __set__(self, obj, value):
         setattr(obj, self._name, int(value))
 
-@dataclass
 class InventoryItem(BaseModel):
-    quantity_on_hand: IntConversionDescriptor = IntConversionDescriptor(default=100.10)
+    quantity_on_hand: IntConversionDescriptor = IntConversionDescriptor(
+        default=100.10,
+        required=True,
+        ui_meta={'label': 'Quantity on Hand'}
+    )
+    price_per_unit: IntConversionDescriptor = IntConversionDescriptor(
+        default=50.75,
+        required=False,
+        ui_meta={'label': 'Price per Unit'}
+    )
 
 item = InventoryItem()
 print(item.quantity_on_hand)  # 100
 item.quantity_on_hand = 2.573   # __set__ converts to integer
 print(item.quantity_on_hand)  # 2
+
+print(item.price_per_unit)  # 50
+item.price_per_unit = 99.99
+print(item.price_per_unit)  # 99
